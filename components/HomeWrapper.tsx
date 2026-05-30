c2297805bec60baac387a419cd94475b9c42e644
SD-CT7 <sddazo.0320@gmail.com>
2026-05-30 16:50:14 +0900

    Update HomeWrapper.tsx

--- components/HomeWrapper.tsx
+++ components/HomeWrapper.tsx
@@ -1,15 +1,17 @@
-import { Suspense } from 'react'
-import { getAllClips } from '@/lib/clips'
-import { courses } from '@/lib/courses'
-import HomeWrapper from '@/components/HomeWrapper'
+'use client'
 
-export default function HomePage() {
-  const clips = getAllClips()
-  return (
-    <main>
-      <Suspense>
-        <HomeWrapper clips={clips} courses={courses} />
-      </Suspense>
-    </main>
-  )
+import { useSearchParams } from 'next/navigation'
+import HomeClient from './HomeClient'
+import type { ClipMeta } from '@/lib/clips'
+import type { Course } from '@/lib/courses'
+
+interface Props {
+  clips: ClipMeta[]
+  courses: Course[]
+}
+
+export default function HomeWrapper({ clips, courses }: Props) {
+  const searchParams = useSearchParams()
+  const initialQuery = searchParams.get('q') ?? ''
+  return <HomeClient clips={clips} courses={courses} initialQuery={initialQuery} />
 }
