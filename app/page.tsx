148c9c68d78170dbb6c7121c8555ddd8878ce25d
SD-CT7 <sddazo.0320@gmail.com>
2026-05-30 16:56:06 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import { Suspense } from 'react'
 import { getAllClips } from '@/lib/clips'
 import { courses } from '@/lib/courses'
-import HomeWrapper from '@/components/HomeWrapper'
+import HomeClient from '@/components/HomeClient'
 
 export default function HomePage() {
   const clips = getAllClips()
   return (
     <main>
       <Suspense>
-        <HomeWrapper clips={clips} courses={courses} />
+        <HomeClient clips={clips} courses={courses} />
       </Suspense>
     </main>
   )
